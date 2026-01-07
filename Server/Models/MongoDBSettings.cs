530da613d247417b30d01812c28b1163d04785c0
Lithi Mgwebi <lithim11@gmail.com>
2026-01-07 14:33:33 +0200

    Removing server

--- Server/Models/MongoDBSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Server.Models
-{
-    public class MongoDBSettings
-    {
-        public string ConnectionUri { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-    }
-}
